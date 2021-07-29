6973d996d83a9d1ee08c7b52c6d4b3966f49f110
Isaac Wingate <ijwingate@outlook.com>
2021-07-29 17:10:04 +1000

    names/course added to menu

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 pygame.font.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1080, 700
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2805ICT - Pacman!")
 
@@ -78,17 +78,39 @@ def menu():
 
 		title_rect=title.get_rect()
 		start_rect=text_start.get_rect()
-		config_rect=title.get_rect()
+		config_rect=text_config.get_rect()
 		quit_rect=text_quit.get_rect()
 
-		# Main Menu Text
+		# Menu Title
 		window.blit(title, (WIDTH/2 - (title_rect[2]/2), 80))
+
+		# Menu Items
 		window.blit(text_start, (WIDTH/2 - (start_rect[2]/2), 240))
 		window.blit(text_config, (WIDTH/2 - (config_rect[2]/2), 300))
 		window.blit(text_quit, (WIDTH/2 - (quit_rect[2]/2), 360))
+
+		#Course and students
+		text_course = text_format("2805ICT - 2021", font, 15, blue)
+		text_student1 = text_format("Harry Rowe", font, 15, blue)
+		text_student2 = text_format("Isaac Wilson", font, 15, blue)
+		text_student3 = text_format("Isaac Wingate", font, 15, blue)
+		text_student4 = text_format("Krittawat Auskulsuthi", font, 15, blue)
+
+		course_rect=title.get_rect()
+		student1_rect=title.get_rect()
+		student2_rect=title.get_rect()
+		student3_rect=title.get_rect()
+		student4_rect=title.get_rect()
+
+		window.blit(text_course, (10, 500))
+		window.blit(text_student1, (10, 540))
+		window.blit(text_student2, (10, 580))
+		window.blit(text_student3, (10, 620))
+		window.blit(text_student4, (10, 660))
+
 		pygame.display.update()
 		clock.tick(FPS)
-		pygame.display.set_caption("Python - Pygame Simple Main Menu Selection")
+		pygame.display.set_caption("2805ICT - Pacman")
 
 
 if __name__ == "__main__":
