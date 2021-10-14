ffb17560c8159c0c0816d696e2c486992f445448
Isaac Wingate <ijwingate@outlook.com>
2021-10-14 11:43:31 +1000

    config menu working

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ class App:
 		self.cellHeight = mHEIGHT//30
 		self.spriteSheet = Spritesheet()
 		self.canWallCollide = True
+		self.randomMaze = False
+		self.config = False
 
 		self.player = Player(self, START_POS_PLAYER, self.spriteSheet)
 		self.sGhosts = []
@@ -108,27 +110,49 @@ class App:
 			if event.type == pygame.QUIT:
 				self.running = False
 			if event.type==pygame.KEYDOWN:
-				if event.key==pygame.K_UP:
-					if self.sel == "config":
-						self.sel = "play"
-					elif self.sel == "exit":
-						self.sel = "config"
-				if event.key==pygame.K_DOWN:
-					if self.sel == "play":
-						self.sel = "config"
-					elif self.sel == "config":
-						self.sel = "exit"
-				if event.key==pygame.K_RETURN:
-					if self.sel =="play":
-						#create walls
-						self.snd_mainMenu.stop()
-						self.createWalls()
-						self.state = 'playing'
-					elif self.sel == "config":
-						print("config")
-					elif self.sel =="exit":
-						self.running = False
-
+				if not self.config:
+					if event.key==pygame.K_UP:
+						if self.sel == "config":
+							self.sel = "play"
+						elif self.sel == "exit":
+							self.sel = "config"
+					if event.key==pygame.K_DOWN:
+						if self.sel == "play":
+							self.sel = "config"
+						elif self.sel == "config":
+							self.sel = "exit"
+					if event.key==pygame.K_RETURN:
+						if self.sel =="play":
+							#create walls
+							self.snd_mainMenu.stop()
+							self.createWalls()
+							self.state = 'playing'
+						elif self.sel == "config":
+							self.config = True
+							if self.randomMaze == True:
+								self.sel='config-random'
+							else:
+								self.sel='config-standard'
+						elif self.sel =="exit":
+							self.running = False
+				else:
+					if event.key==pygame.K_UP:
+						if self.sel == "config-random":
+							self.sel = "config-standard"
+						elif self.sel == "config-back":
+							self.sel = "config-random"
+					if event.key==pygame.K_DOWN:
+						if self.sel == "config-standard":
+							self.sel = "config-random"
+						elif self.sel == "config-random":
+							self.sel = "config-back"
+					if event.key==pygame.K_RETURN:
+						if self.sel =="config-random":
+							self.randomMaze = True;
+						elif self.sel == "config-standard":
+							self.randomMaze = False
+						self.sel = 'config'
+						self.config = False
 
 	def mMenu_update(self):
 		pass
@@ -138,18 +162,32 @@ class App:
 
 		self.screen.blit(self.title, (WIDTH/2-300, 120))
 
-		if self.sel == 'play':
-			self.drawText('Play',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+		if self.config:
+			if self.sel == 'config-standard':
+				self.drawText('Standard',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Standard',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, white, True)
+			if self.sel == 'config-random':
+				self.drawText('Random',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Random',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, white, True)
+			if self.sel == 'config-back':
+				self.drawText('Back',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Back',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, white, True)
 		else:
-			self.drawText('Play',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, white, True)
-		if self.sel == 'config':
-			self.drawText('Configure',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, yellow, True)
-		else:
-			self.drawText('Configure',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, white, True)
-		if self.sel == 'exit':
-			self.drawText('Exit',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, yellow, True)
-		else:
-			self.drawText('Exit',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, white, True)
+			if self.sel == 'play':
+				self.drawText('Play',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Play',self.screen, [WIDTH//2, HEIGHT//2], MENU_FONT, MENU_FONT_LARGE, white, True)
+			if self.sel == 'config':
+				self.drawText('Configure',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Configure',self.screen, [WIDTH//2, HEIGHT//2+65], MENU_FONT, MENU_FONT_LARGE, white, True)
+			if self.sel == 'exit':
+				self.drawText('Exit',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, yellow, True)
+			else:
+				self.drawText('Exit',self.screen, [WIDTH//2, HEIGHT//2+130], MENU_FONT, MENU_FONT_LARGE, white, True)
 
 
 		#Course and students
@@ -216,8 +254,11 @@ class App:
 		for ghost in self.dGhosts:
 			ghost.draw()
 
-		for wall in self.walls:
-			wall.draw()
+		if self.randomMaze:
+			pass
+		else:
+			for wall in self.walls:
+				wall.draw()
 
 		pygame.display.update()
 
